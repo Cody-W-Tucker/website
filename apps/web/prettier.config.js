15266cf349c0840acc841aa8f229c55883db9298
Cody W Tucker <cody@tmvsocial.com>
2025-04-24 17:01:35 -0500

    Update Prettier configuration and remove unused logo-dark.svg file

--- apps/web/prettier.config.js
+++ apps/web/prettier.config.js
@@ -4,8 +4,18 @@
  */
 const config = {
   printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
   semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
   bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
 };
 
 export default config;
